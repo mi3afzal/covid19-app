377b514fecb2aa3b16fdd37487287097a603b918
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2020-07-10 03:15:08 +0500

    Combine Data in four cards

--- src/Components/DataCards.js
+++ src/Components/DataCards.js
@@ -5,15 +5,14 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
-		margin: '50px',
+		margin: '50px auto',
+		width: '80%',
 	},
 	card: {
-		minWidth: 275,
-		textAlign: 'center',
-	  },
-	  title: {
-		fontSize: 14,
-	  },
+		
+	},
+
+	
 }));
 
 export default function DataCards() {
@@ -34,25 +33,52 @@ export default function DataCards() {
 
 	return (
 		<div className={classes.root}>
-			<Grid container spacing={3}>
+			<Grid container spacing={3} justify="center" alignItems="stretch">
 
-				{Object.keys(globalData).map( (key, index) => {
-					return (
-						<Grid item xs={12} md={3} key={index}>
-							<Card className={classes.card}>
-								<CardContent>
-									<Typography className={classes.title} color="textSecondary" gutterBottom>
-										{key.replace(/_/g, ' ').toUpperCase()}
-									</Typography>
-									<Typography variant="h5" component="h2">
-										{globalData[key]}
-									</Typography>
-								</CardContent>
-							</Card>
-						</Grid>
-					);
-				} )}
+				<Grid item xs={12} md={3}>
+					<Card className={`${classes.card} confirmed`}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Confirmed</Typography>
+							<Typography variant="h5" component="h2">{globalData.total_cases}</Typography>
+							<Typography color="secondary">+{globalData.total_new_cases_today} New</Typography>
+							<Typography variant="body2" component="p">Number of confirmed COVID-19 cases</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
 
+				<Grid item xs={12} md={3}>
+					<Card className={`${classes.card} active`}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Active</Typography>
+							<Typography variant="h5" component="h2">{globalData.total_active_cases}</Typography>
+							<Typography color="secondary">{globalData.total_serious_cases} Critical</Typography>
+							<Typography variant="body2" component="p">Number of active COVID-19 cases</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} md={3}>
+					<Card className={`${classes.card} recovered`}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Recovered</Typography>
+							<Typography variant="h5" component="h2">{globalData.total_recovered}</Typography>
+							<Typography className="newRecoveries">+{globalData.total_unresolved} Unresolved</Typography>
+							<Typography variant="body2" component="p">Number of recoveries from COVID-19</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+
+				<Grid item xs={12} md={3}>
+					<Card className={`${classes.card} confirmed`}>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>Deaths</Typography>
+							<Typography variant="h5" component="h2">{globalData.total_deaths}</Typography>
+							<Typography color="secondary">+{globalData.total_new_deaths_today} New</Typography>
+							<Typography variant="body2" component="p">Number of deaths from COVID-19</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+				
 			</Grid>
 		</div>
 	);
