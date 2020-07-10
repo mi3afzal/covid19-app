2a5ca5ba2b45b33989b88575ab0876199a1ccc00
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2020-07-11 00:04:37 +0500

    fix deployment issue - unused variable

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 		<div className="app">
 			<Header />
 			<DataCards />
-			<CountrySelector setCountry={setCountry} />
+			<CountrySelector country={country} setCountry={setCountry} />
 			<DataChart />
 			<Map />
 			<Footer />
