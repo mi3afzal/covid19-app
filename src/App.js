424088180921833788a710ae570fa1f1eb56a126
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2020-07-10 23:55:50 +0500

    Country selection is completed.
    need to change API provider for data consistency.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
 import Header from './Components/Header';
 import DataCards from './Components/DataCards';
+import CountrySelector from './Components/CountrySelector';
 import DataChart from './Components/DataChart';
 import Map from './Components/Map';
 import Footer from './Components/Footer';
 
+
 function App() {
+	const [country, setCountry] = useState();
+
 	return (
 		<div className="app">
 			<Header />
 			<DataCards />
+			<CountrySelector setCountry={setCountry} />
 			<DataChart />
 			<Map />
 			<Footer />
--- src/Components/CountrySelector.js
+++ src/Components/CountrySelector.js
@@ -1,1 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
+export default function CountrySelector(props) {
+
+	const [countries, setCountries] = useState([]);
+	useEffect(() => {
+		const fetchAPI = async () => {
+			const response = await fetch('https://covid19.mathdro.id/api/countries');
+			let data = await response.json();
+			console.log(data);
+
+			const countriesObj = data.countries.map( (country) => country.name );
+			setCountries(countriesObj);
+		};
+		fetchAPI();
+	}, []);
+
+	return (
+		<FormControl className="formControl">
+			<NativeSelect defaultValue="" onChange={(e) => props.setCountry(e.target.value)}>
+				<option value="">Global</option>
+				{countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+			</NativeSelect>
+		</FormControl>
+	);
+};
