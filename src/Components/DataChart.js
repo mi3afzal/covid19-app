99e635bb4718e46107f73a8a0a9426ab0b9c6009
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2020-07-10 22:13:13 +0500

    Fix chart

--- src/Components/DataChart.js
+++ src/Components/DataChart.js
@@ -1,44 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
-const data = {
-	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	datasets: [
-		{
-			label: 'My First dataset',
-			fill: false,
-			lineTension: 0.1,
-			backgroundColor: 'rgba(75,192,192,0.4)',
-			borderColor: 'rgba(75,192,192,1)',
-			borderCapStyle: 'butt',
-			borderDash: [],
-			borderDashOffset: 0.0,
-			borderJoinStyle: 'miter',
-			pointBorderColor: 'rgba(75,192,192,1)',
-			pointBackgroundColor: '#fff',
-			pointBorderWidth: 1,
-			pointHoverRadius: 5,
-			pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-			pointHoverBorderColor: 'rgba(220,220,220,1)',
-			pointHoverBorderWidth: 2,
-			pointRadius: 1,
-			pointHitRadius: 10,
-			data: [65, 59, 80, 81, 56, 55, 40]
-		}
-	]
-};
-
 export default function DataChart(){
 
-	const [chartData, setChartlData] = useState(data);
+	const [chartData, setChartlData] = useState({});
 	useEffect(()=>{
-		async function fetchData(run){
-			if(run){
-				const response = await fetch('https://thevirustracker.com/timeline/map-data.json');
-				let data = await response.json();
-				console.log(data);
-				setChartlData(data);
+		async function fetchData()
+		{
+			const response = await fetch('https://covid19.mathdro.id/api/daily');
+			let data = await response.json();
+			console.log(data);
+			
+			let chart_data = data.map(	(obj) => ({ 
+				confirmed: obj.confirmed.total, 
+				deaths: obj.deaths.total, 
+				date: obj.reportDate 
+			}) );
+			//console.log(chart_data); // {confirmed: 555, deaths: 17, date: "2020-01-22"}
+
+			let chartObj = {
+				labels: chart_data.map( ({ date }) => date ),
+				datasets: [
+					{
+						data: chart_data.map((data) => data.confirmed),
+						label: 'Infected',
+						borderColor: '#3333ff',
+						fill: true,
+					}, {
+						data: chart_data.map((data) => data.deaths),
+						label: 'Deaths',
+						borderColor: 'red',
+						backgroundColor: 'rgba(255, 0, 0, 0.5)',
+						fill: true,
+					},
+				]
 			}
+			//console.log(chartObj);
+			
+			setChartlData(chartObj);
 		}
 		fetchData();
 	}, []);
